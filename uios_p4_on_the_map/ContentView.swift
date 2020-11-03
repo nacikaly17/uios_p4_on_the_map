4d416a2a483d425e0d3e4e4bca03e2286490fbc4
Naci Kalyoncu <naci.kalyoncu@gmail.com>
2020-11-03 08:44:57 +0100

    Intitial Submit

---
+++ uios_p4_on_the_map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uios_p4_on_the_map
+//
+//  Created by Naci Kalyoncu on 03.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
